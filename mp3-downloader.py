6db6b068f46389176aaa94123a40d9c8ddeb5e0e
unknown <dominik.majdandzic@gmail.com>
2023-11-29 18:31:43 +0100

    added downloading mp3 from search term

--- mp3-downloader.py
+++ mp3-downloader.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from youtube_search import YoutubeSearch
 import os
 
 
@@ -13,8 +14,27 @@ def download_mp3_from_link(url, destination):
     os.rename(out_file, new_file)
 
 
-if __name__ == "__main__":
-    destination = input("Enter save folder: ")
-    url = input("Enter YouTube video URL: ")
+def download_mp3_from_search_term(search_term, destination):
+    result = YoutubeSearch(search_terms=search_term, max_results=1).to_dict()
+    url = "https://www.youtube.com" + result[0]["url_suffix"]
 
-    download_mp3_from_link(url, destination)
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+
+    out_file = video.download(output_path=destination)
+
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+
+
+if __name__ == "__main__":
+    # destination = input("Enter save folder: ")
+    # url = input("Enter YouTube video URL: ")
+
+    # download_mp3_from_link(url, destination)
+
+    destination = input("Enter save folder: ")
+    search_term = input("Enter search term: ")
+
+    download_mp3_from_search_term(search_term, destination)
