b3130b66c3331e40a12fcaa2d34d2efff96bf723
unknown <dominik.majdandzic@gmail.com>
2023-12-01 11:34:33 +0100

    added basic error handling

--- mp3_downloader.py
+++ mp3_downloader.py
@@ -4,31 +4,40 @@ import os
 
 
 def download_mp3_from_link(url, destination):
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
+    try:
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
 
-    out_file = video.download(output_path=destination)
+        out_file = video.download(output_path=destination)
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
-    os.rename(out_file, new_file)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + ".mp3"
+        if os.path.exists(new_file):
+            os.remove(out_file)
+        else:
+            os.rename(out_file, new_file)
+    except Exception:
+        print("Could not download file: " + url)
 
 
 def download_mp3_from_search_term(search_term, destination):
-    result = YoutubeSearch(search_terms=search_term, max_results=1).to_dict()
-    url = "https://www.youtube.com" + result[0]["url_suffix"]
+    try:
+        result = YoutubeSearch(search_terms=search_term, max_results=1).to_dict()
+        url = "https://www.youtube.com" + result[0]["url_suffix"]
 
-    yt = YouTube(url)
-    video = yt.streams.filter(only_audio=True).first()
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
 
-    out_file = video.download(output_path=destination)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
+        out_file = video.download(output_path=destination)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + ".mp3"
 
-    if os.path.exists(new_file):
-        os.remove(out_file)
-    else:
-        os.rename(out_file, new_file)
+        if os.path.exists(new_file):
+            os.remove(out_file)
+        else:
+            os.rename(out_file, new_file)
+    except Exception:
+        print("Could not download file: " + search_term)
 
 
 if __name__ == "__main__":
