6a1b5a7ca7ef33cc587404c30a7594ab62a514b2
unknown <dominik.majdandzic@gmail.com>
2023-12-01 11:38:06 +0100

    removed redundant code

--- mp3_downloader.py
+++ mp3_downloader.py
@@ -41,7 +41,4 @@ def download_mp3_from_search_term(search_term, destination):
 
 
 if __name__ == "__main__":
-    destination = input("Enter save folder: ")
-    search_term = input("Enter search term: ")
-
-    download_mp3_from_search_term(search_term, destination)
+    pass
